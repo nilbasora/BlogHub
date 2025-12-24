2bc8d8b9b96309c646f3891950aed5a3ddc72108
nilbasora <nilbasora@gmail.com>
2025-12-24 14:45:23 +0100

    fix: Corrected the case of the App.css import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./router";
-import "./app.css";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
